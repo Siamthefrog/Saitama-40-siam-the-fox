d48cafd2950a6b5dd7f6645c092f6dc417489534
Siam the frog <164476403+Siamthefrog@users.noreply.github.com>
2024-05-07 00:49:32 +0600

    Update Up.js

--- scripts/cmds/Up.js
+++ scripts/cmds/Up.js
@@ -1,48 +1,99 @@
-৳cmd install up.js const moment = require('moment-timezone');
-moment.tz.setDefault('Asia/Dhaka');
-const os = require('os');
-
 module.exports = {
-  config: {
-    name: 'up',
-    version: '1.0.0',
-    author: 'Siam the frog>🐸',
-    countDown: 0,
-    role: 0,
-    category: 'uptime',
-    shortDescription: 'up ck bot', 
-    longDescription: 'Bot uptime cheak',
-    guide: {
-      en: '{pn} ',
-    },
-  },
-  
-onStart: async function ({ message, event, usersData, threadsData }) {
-     const uptime = process.uptime();
-     const startTime = Date.now();
-     const hours = Math.floor(uptime / 3600);
-     const munites = Math.floor((uptime % 3600) / 60);
-     const second = Math.floor(uptime % 60);
-     
-     const arif = `${hours} hours ${munites} munites ${second} second`;
-     
-     const url = await global.utils.getStreamFromURL("https://tinyurl.com/2c6fp9ar");
-   
-     const now = moment();
-     const riley = now.format('DD-MMMM-Y/hh.mm.ss A');
-     
-     const veli = `${Math.round(os.totalmem() / (1024 * 1024 * 1024))} GB`;
-     const putra = await usersData.getAll();
-     const loufi = await threadsData.getAll(); 
-     const luxion = `${os.type()} ${os.release()}`;
-     const rizky = `${os.cpus()[0].model} (${os.cpus().length} cores)`
-     
-     const endTime = Date.now();
-     const raffa = endTime - startTime;
-     
-     const frog = `Bot ping: ${raffa}\n━━━━━━━━━━━\nUptime: ${arif}\n━━━━━━━━━━━\n\nDate: ${riley}\n━━━━━━━━━━`
 
-     message.reply("Wait boss🐸", event.messageID);
-     message.reply({ body: frog, attachment: url }, event.threadID);
+  config: {
+
+    name: 'up',
+
+    version: '1.0.0',
+
+    author: 'Siam the frog>🐸',
+
+    countDown: 0,
+
+    role: 0,
+
+    category: 'display see the current uptime',
+
+    shortDescription: 'display the current Bot uptime', 
+
+    longDescription: 'Check the current uptime',
+
+    guide: {
+
+      en: '{pn} ',
+
+    },
+
   },
+
+  
+
+onStart: async function ({ api, event, message }) {
+
+    const senderID = event.senderID;
+    
+    
+	const frogs = [
+      "Hey hello🐸",
+      "hello boss",
+      "hey bokacoda how are you 🐸",
+      "Hey xudanir fua kaiss ho 🐸",
+      "Do you know your place baka",
+      "yo wai mo",
+      "Bitch",
+      "Hello sauar beta welcome to back bc",
+      "how are you nigga? ",
+      "how can i fuck your wife boss"
+    ];
+    const randomfrogs = frogs[Math.floor(Math.random() * frogs.length)];
+    
+
+    const loadingMessage = await message.reply({
+
+      body: "Please wait🐸",
+
+    });
+
+    
+
+     const frog = await global.utils.getStreamFromURL( "https://drive.google.com/uc?export=download&id=1i7WHd9KePv61NLESwqMV3zP6Vuq8Gehb");
+
+     const os = require("os");
+
+	     const uptime = process.uptime();
+         const seconds = Math.floor(uptime % 60);
+ 	    const minutes = Math.floor((uptime / 60) % 60);
+  	   const hours = Math.floor((uptime / (60 * 60)) % 24);
+  	   const days = Math.floor(uptime / (60 * 60 * 24));
+  	   const siam = `${days} days ${hours} hours ${minutes} minutes ${seconds} second`;
+         
+         const totalMemory = `Total Memory: ${Math.round(os.totalmem() / (1024 * 1024 * 1024))} GB`;
+         
+
+				const osInfo = {
+						platform: os.platform(),
+						architecture: os.arch()
+				};
+                
+                const currentDate = new Date();
+      const options = { year: "numeric", month: "numeric", day: "numeric" };
+      const date = currentDate.toLocaleDateString("en-US", options);
+      const time = currentDate.toLocaleTimeString("en-US", {
+        timeZone: "Asia/Dhaka",
+        hour12: true,
+      });
+                
+     const Body = `Up: ${siam}\nRam: ${Math.round(os.totalmem() / (1024 * 1024 * 1024))} GB\nOperating System: ${osInfo.platform}\ncpu: ${osInfo.architecture}\nCores: ${os.cpus().length}\nDate: ${date}\nTime ${time}\n${randomfrogs}`
+
+
+     message.reply({ body: Body, attachment: frog }, event.threadID);
+
+     setTimeout(() => {
+
+       api.unsendMessage(loadingMessage.messageID);
+
+      }, 2000);
+
+   },
+
 };
